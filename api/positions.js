684db435702ba0d528a171c68611f7273dc91fab
Mikael Nore <mikaeln@tsoslo.no>
2025-02-19 15:43:46 +0100

    totalt renovering

--- api/positions.js
+++ api/positions.js
@@ -8,28 +8,28 @@ const apiConfigurations = [
         apiKey: process.env.API_KEY_1,
         logo: '/logo1.png',
         company: 'Transportsentralen Oslo',
-        vehicleFile: 'tsoslo.json', // Filen for bilinformasjon
+        vehicleFile: 'tsoslo.json',
     },
     {
         url: process.env.API_URL_2,
         apiKey: process.env.API_KEY_2,
         logo: '/logo2.png',
         company: 'TS Oslo Budtjenester',
-        vehicleFile: 'tsoslobud.json', // Filen for bilinformasjon
+        vehicleFile: 'tsoslobud.json',
     },
     {
         url: process.env.API_URL_3,
         apiKey: process.env.API_KEY_3,
         logo: '/logo3.png',
         company: 'Moss Transportforum',
-        vehicleFile: 'mtf.json', // Filen for bilinformasjon
+        vehicleFile: 'mtf.json',
     },
     {
         url: process.env.API_URL_4,
         apiKey: process.env.API_KEY_4,
         logo: '/logo4.png',
         company: 'Blå Kurér',
-        vehicleFile: 'blakurer.json', // Filen for bilinformasjon
+        vehicleFile: 'blakurer.json',
     },
 ];
 
@@ -58,12 +58,10 @@ module.exports = async (req, res) => {
 
         for (const config of apiConfigurations) {
             try {
-                // Les bilinformasjon fra den aktuelle JSON-filen
                 const filePath = path.join(process.cwd(), 'data', config.vehicleFile);
                 const fileContents = await fs.readFile(filePath, 'utf8');
                 const vehiclesArray = JSON.parse(fileContents);
 
-                // Gjør bilinformasjonen om til et oppslag (object) basert på kjøretøynummer
                 const vehiclesData = vehiclesArray.reduce((acc, vehicle) => {
                     acc[vehicle.number] = vehicle;
                     return acc;
@@ -83,7 +81,6 @@ module.exports = async (req, res) => {
                 const data = await response.json();
                 console.log(`Data fetched from ${config.url}:`, data);
 
-                // Legg til logoer, firma og kjøretøyinfo
                 const vehiclesWithLogos = data.map(vehicle => {
                     const vehicleNumber = ensureString(vehicle.number);
                     const vehicleData = vehiclesData[vehicleNumber] || {};
